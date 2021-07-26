19f238f08d0b35369e493423b8078a88c516a7ee
Charles Fousse <cefsdp@gmail.com>
2021-07-26 21:59:42 +0200

    Verify presence and uniqueness of CodeJE in Juniors Model + Created JuniorsController

---
+++ app/controllers/juniors_controller.rb
@@ -0,0 +1,2 @@
+class JuniorsController < ApplicationController
+end
--- app/models/junior.rb
+++ app/models/junior.rb
@@ -1,2 +1,4 @@
 class Junior < ApplicationRecord
+  validates_presence_of :CodeJE, on: :create, message: "Vous devez avoir un code"
+  validates_uniqueness_of :CodeJE, on: :create, message: "Ce code ne peut être utilisé"
 end
