1a25dd34eac9f7a4883e65ffd0338092c530a89b
Charles Fousse <cefsdp@gmail.com>
2021-07-30 22:17:23 +0200

    removed membres model

--- app/models/membre.rb
+++
@@ -1,4 +0,0 @@
-class Membre < ApplicationRecord
-  belongs_to :junior
-  belongs_to :user
-end
--- db/migrate/20210730201429_add_admin_to_membres.rb
+++
@@ -1,5 +0,0 @@
-class AddAdminToMembres < ActiveRecord::Migration[6.1]
-  def change
-    add_column :membres, :admin, :boolean
-  end
-end
