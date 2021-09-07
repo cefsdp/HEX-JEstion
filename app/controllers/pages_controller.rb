6e20d03d5a467803599e9a3056267441deffc1ce
Charles Fousse <cefsdp@gmail.com>
2021-09-07 20:47:59 +0200

    New front for Sign up and Sign in + Redirect_to junior_path from home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    # raise
+    if current_user.nil?
+      redirect_to new_user_session_path
+    else
+      redirect_to junior_path(current_user.junior)
+    end
   end
 end
