db7495b5dfead3c7c29455402ed4f4a3345e6e20
Charles Fousse <cefsdp@gmail.com>
2021-09-28 16:32:26 +0200

    CSS - Tableau, Show Adherent Documents, Added Fin Validite Document

--- app/controllers/document_adherents_controller.rb
+++ app/controllers/document_adherents_controller.rb
@@ -12,6 +12,7 @@ class DocumentAdherentsController < ApplicationController
                                              date_debut_validite: @adherentDocumentParams[:date_debut_validite],
                                              validite: 'pending',
                                              document: @adherentDocumentParams[:document])
+    @documentAdherent.date_fin_validite = calcul_date_fin_validite(@documentAdherent)
     authorize @documentAdherent
     if @documentAdherent.save
       flash[:success] = "Object successfully created"
@@ -22,10 +23,47 @@ class DocumentAdherentsController < ApplicationController
     end
   end
 
-  private
 
+  def update
+    @user = current_user
+    @adherentDocumentParams = document_adherent_params
+    @juniorConfigDocAdherent = ConfigDocAdherent.where(junior_configuration_id: JuniorConfiguration.find_by(junior_id: @user.junior_id).id).find_by(nom: @adherentDocumentParams[:nom])
+    @documentAdherent = DocumentAdherent.find(params[:id])
+    authorize @documentAdherent
+      if @documentAdherent.update(nom: @adherentDocumentParams[:nom],
+                                  obligatoire: @juniorConfigDocAdherent.obligatoire,
+                                  archive: false,
+                                  date_debut_validite: @adherentDocumentParams[:date_debut_validite],
+                                  date_fin_validite: calcul_date_fin_validite(@documentAdherent),
+                                  validite: @adherentDocumentParams[:validite],
+                                  document: @adherentDocumentParams[:document])
+        flash[:success] = "Object was successfully updated"
+        redirect_to @documentAdherent
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+  
+  
+  private
+  
   def document_adherent_params
     params.require(:document_adherent).permit(:nom, :obligatoire, :date_debut_validite, :validite, :archive, :document)
     # validite: pending, valid, invalid
+  end
+  
+  def calcul_date_fin_validite(document_adh)
+    config_doc_adh = document_adh.adherent.user.junior.config_doc_adherents.find_by(nom: document_adh.nom)
+    duree_validite = config_doc_adh.duree_validite
+    format_duree_validite = config_doc_adh.format_duree_validite
+    case format_duree_validite
+    when 'an'
+      document_adh.date_debut_validite + duree_validite.to_i.year
+    when 'mois'
+      document_adh.date_debut_validite + duree_validite.to_i.month
+    when 'jours'
+      document_adh.date_debut_validite + duree_validite.to_i.day
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_212923) do
+ActiveRecord::Schema.define(version: 2021_09_28_123947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 2021_09_24_212923) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "date_debut_validite"
+    t.string "raison_invalid"
+    t.date "date_fin_validite"
     t.index ["adherent_id"], name: "index_document_adherents_on_adherent_id"
   end
 
